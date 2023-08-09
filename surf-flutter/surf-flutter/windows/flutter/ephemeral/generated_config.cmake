f302ecc6db687bff7bfacf52dda9d0e72dc9a198
Ali Haly <ali.beybalaev@gmail.com>
2023-08-09 18:31:12 +0300

    task4.1

---
+++ surf-flutter/surf-flutter/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\surf-course\\flutter\\surf-flutter\\surf-flutter" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\surf-course\\flutter\\surf-flutter\\surf-flutter"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\surf-course\\flutter\\surf-flutter\\surf-flutter\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\surf-course\\flutter\\surf-flutter\\surf-flutter"
+  "FLUTTER_TARGET=C:\\surf-course\\flutter\\surf-flutter\\surf-flutter\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jZGJlZGE3ODhhMjkzZmEyOTY2NWRjM2ZhM2Q2ZTYzYmQyMjFjYjBkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\surf-course\\flutter\\surf-flutter\\surf-flutter\\.dart_tool\\package_config.json"
+)
